c3d3b05ca02355dbaf715ba9790fc8a4f5a14ca5
Andrei Georgescu <ageorgescu@mail.sfsu.edu>
2022-01-11 21:29:35 -0800

    Cleaned up posture.py and added flags for different deviation algorithms

--- src/app.py
+++ src/app.py
@@ -8,9 +8,18 @@ class Application(rumps.App):
         super().__init__("Posture Watcher", quit_button=None)
         self.pw = PostureWatcher()
 
-    @rumps.clicked("Set Base Posture")
+    @rumps.clicked("Set base posture")
     def set_base_posture(self, _):
         self.pw.set_base_posture()
+
+    @rumps.clicked("Clear base posture")
+    def clear_base_posture(self, _):
+        self.pw.base_posture = None
+
+    @rumps.clicked("Quit")
+    def quit(self, _):
+        self.pw.stop()
+        rumps.quit_application()
 
     @rumps.timer(5)
     def check_posture(self, _):
@@ -19,7 +28,7 @@ class Application(rumps.App):
     @rumps.timer(1)
     def update_title(self, _):
         if not self.pw.base_posture:
-            self.title += "⚠️ Please set your base posture."
+            self.title = "⚠️ Please set your base posture."
         else:
             cd = self.pw.deviation.current_deviation
             self.title = "Posture Watcher: "
@@ -29,11 +38,6 @@ class Application(rumps.App):
                 self.title += f"⚠️ Improve your posture! ({cd}%)"
             else:
                 self.title += f"⛔️ Fix your posture! ({cd}%)"
-
-    @rumps.clicked("Quit")
-    def quit(self, _):
-        self.pw.stop()
-        rumps.quit_application()
 
 
 if __name__ == "__main__":
--- src/posture.py
+++ src/posture.py
@@ -1,6 +1,5 @@
 from detector import PoseDetector, PoseLandmarks
 from deviation import Deviation
-from threading import Thread
 from logger import Logger
 import cv2
 import logger
@@ -25,6 +24,7 @@ class PostureWatcher:
     """
 
     def __init__(self,
+                 deviation_algorithm=2,
                  deviation_interval=5,
                  deviation_adjustment=5,
                  deviation_threshold=25,
@@ -33,6 +33,7 @@ class PostureWatcher:
                  debug=True,):
         """
         Initializes the PostureWatcher.
+        :param deviation_algorithm: The algorithm used to calculate the deviation.
         :param deviation_interval: The interval in seconds between checking for deviation
         :param deviation_adjustment: The amount of deviation to allow before triggering an alert
         :param deviation_threshold: The threshold at which a deviation should trigger an alert
@@ -46,6 +47,7 @@ class PostureWatcher:
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self.last_fps_calc_timestamp = 0
+        self.deviation_algorithm = deviation_algorithm
         self.deviation_interval = deviation_interval
         self.deviation_adjustment = deviation_adjustment
         self.base_posture = base_posture
@@ -60,12 +62,10 @@ class PostureWatcher:
         if not self.base_posture:
             return
 
-        def execute():
-            _, img = self.cap.read()
-            img, _ = self.detector.find_pose(img)
-            self.deviation.current_deviation = self._get_deviation_from_base_posture()
-            self._handle_deviation()
-        Thread(target=execute).start()
+        _, img = self.cap.read()
+        img, _ = self.detector.find_pose(img)
+        self.deviation.current_deviation = self._get_deviation_from_base_posture()
+        self._handle_deviation()
 
     def stop(self):
         """
@@ -85,25 +85,12 @@ class PostureWatcher:
             r_shoulder = lm[12]
             self.base_posture = BasePosture(nose, mouth_l, mouth_r, l_shoulder, r_shoulder)
 
-    def _log_deviation(self):
-        cd = self.deviation.current_deviation
-        cdma = self.deviation.moving_average
-        buffer = self.deviation.current_buffer
-
-        logger.clear_console()
-
-        if self.deviation.has_deviated():
-            self.logger.notify(f"Detected deviation from base posture by {cd}%", color='red', with_sound=True)
-        else:
-            self.logger.notify(f"You are doing great! {cd}% (MA: {cdma}%)", color='green')
-
-        if self.debug:
-            self.logger.notify(f"Deviation MA: {cdma}%", color='grey')
-            self.logger.notify(f"Deviation buffer: {buffer}", color='grey')
-
-    def _get_deviation_from_base_posture(self):
+    def _get_deviation_from_base_posture(self, algorithm_version: int = 1) -> int or None:
         """
         Calculates the deviation from the base posture as a percentage
+        :param algorithm_version: The algorithm version to use (int).
+                                   1: Uses body and face to calculate deviation
+                                   2: Uses only face to calculate deviation
         :returns: float from 0-100, 100 being the most deviant from the base posture
         """
         if self.base_posture is None:
@@ -111,28 +98,64 @@ class PostureWatcher:
 
         _, img = self.cap.read()
         _, lm = self.detector.find_pose(img)
+        deviation = 100
 
         if not lm:  # No pose found
-            return 100
+            return deviation
 
+        # Nose and mouth are used for both algorithms so its out here.
+        # TODO: Make this more efficient and clean, this function is too cluttered
         nose = abs(self.base_posture.nose.x - lm[0].x) + abs(self.base_posture.nose.y - lm[0].y) + abs(
             self.base_posture.nose.z - lm[0].z)
-        mouth_l = abs(self.base_posture.mouth_left.x - lm[9].x) + abs(self.base_posture.mouth_left.y - lm[9].y) + abs(
-            self.base_posture.mouth_left.z - lm[9].z)
-        mouth_r = abs(self.base_posture.mouth_right.x - lm[10].x) + abs(self.base_posture.mouth_right.y - lm[10].y) + \
-            abs(self.base_posture.mouth_right.z - lm[10].z)
-        l_shoulder = abs(self.base_posture.left_shoulder.x - lm[11].x) + abs(self.base_posture.left_shoulder.y - lm[11].y) + \
-            abs(self.base_posture.left_shoulder.z - lm[11].z)
-        r_shoulder = abs(self.base_posture.right_shoulder.x - lm[12].x) + abs(self.base_posture.right_shoulder.y - lm[12].y) + \
-            abs(self.base_posture.right_shoulder.z - lm[12].z)
+        mouth_l = abs(self.base_posture.mouth_left.x - lm[9].x) + abs(
+            self.base_posture.mouth_left.y - lm[9].y) + abs(self.base_posture.mouth_left.z - lm[9].z)
+        mouth_r = abs(self.base_posture.mouth_right.x - lm[10].x) + abs(
+            self.base_posture.mouth_right.y - lm[10].y) + abs(self.base_posture.mouth_right.z - lm[10].z)
 
-        deviation = int(
-            ((nose + mouth_l + mouth_r + l_shoulder + r_shoulder) / (self.base_posture.nose.x +
-             self.base_posture.mouth_left.x + self.base_posture.mouth_right.x + self.base_posture.left_shoulder.x +
-             self.base_posture.right_shoulder.x) * 100))
+        if algorithm_version == 1:
+            """
+            Algorithm 1: Utilities shoulders in addition to the face to track posture.
+            """
+            l_shoulder = abs(self.base_posture.left_shoulder.x - lm[11].x) + abs(self.base_posture.left_shoulder.y - lm[11].y) + \
+                abs(self.base_posture.left_shoulder.z - lm[11].z)
+            r_shoulder = abs(self.base_posture.right_shoulder.x - lm[12].x) + abs(self.base_posture.right_shoulder.y - lm[12].y) + \
+                abs(self.base_posture.right_shoulder.z - lm[12].z)
 
-        adjusted_deviation = 100 if deviation >= 100 else deviation - self.deviation_adjustment
+            deviation = int(
+                ((nose + mouth_l + mouth_r + l_shoulder + r_shoulder) / (self.base_posture.nose.x +
+                 self.base_posture.mouth_left.x + self.base_posture.mouth_right.x + self.base_posture.left_shoulder.x +
+                 self.base_posture.right_shoulder.x) * 100))
+        elif algorithm_version == 2:
+            """
+            Algorithm 2: Utilities only the face to track posture.
+            """
+            deviation = int(
+                ((nose + mouth_l + mouth_r) / (self.base_posture.nose.x + self.base_posture.mouth_left.x +
+                                               self.base_posture.mouth_right.x) * 100))
+
+        adjusted_deviation = 100 if deviation >= 100 else int(deviation - self.deviation_adjustment)
         return adjusted_deviation
+
+    def _log_deviation(self, cd: int, cdma: float, buffer: int):
+        """
+        Logs the deviation using the built-in logging utility.
+        :return: None
+        """
+        logger.clear_console()
+
+        if self.deviation.has_deviated():
+            self.logger.notify(f"Detected deviation from base posture by {cd}%", color='red', with_sound=True)
+        else:
+            if cd < 25:
+                self.logger.notify(f"✅ Great posture! {cd}% (MA: {cdma}%)", color='green')
+            elif cd < 35:
+                self.logger.notify(f"⚠️ Improve your posture! {cd}% (MA: {cdma}%)", color='yellow')
+            else:
+                self.logger.notify(f"️ Fix your posture! {cd}% (MA: {cdma}%)", color='red')
+
+        if self.debug:
+            self.logger.notify(f"Deviation MA: {cdma}%", color='white')
+            self.logger.notify(f"Deviation buffer: {buffer}", color='white')
 
     def _handle_deviation(self):
         """
@@ -141,4 +164,8 @@ class PostureWatcher:
         if self.deviation.current_deviation is None:
             return
 
-        self._log_deviation()
+        cd = self.deviation.current_deviation
+        cdma = self.deviation.moving_average
+        buffer = self.deviation.current_buffer
+
+        self._log_deviation(cd, cdma, buffer)
